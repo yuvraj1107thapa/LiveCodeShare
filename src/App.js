5345ce12340008a4a7d14712c86f1d0896016c96
Yuvraj Thapa <yuvrajthapa692@gmail.com>
2024-02-12 23:02:13 +0530

    fix logging

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import { React, useState } from 'react';
+import Room from './components/room';
+import {
+  Routes,
+  Route,
+  useNavigate
+} from "react-router-dom";
+import uuid from 'react-uuid';
+import LandingPage from './components/landingPage';
+import Main from './views/Main';
+import FindDev from './views/FindDev';
+import NotFound from './components/navbar/NotFound';
+
+function App() {
+  //JOIN VIDEO CHAT ROOM 
+  const [joined, setJoined] = useState(false)
+  const [userID, setUserID] = useState('')
+  const [userName, setUserName] = useState('')
+  const [roomName, setRoomName] = useState('')
+  const [invitationLink, setinvitationLink] = useState('')
+  const navigate = useNavigate();
+
+  const createRoom = (e) => {
+    e.preventDefault();
+    const hasSpaces = Boolean(userName.indexOf(' '))
+    if (roomName === undefined || userName === undefined || !hasSpaces) {
+      setJoined(false)
+      navigate('/invalid')
+    }
+    setUserID(uuid())
+    setJoined(true)
+    const roomNameWithoutSpace = roomName.replace(/ /g, '-');
+    const room_id = uuid().substring(0, 8) + '@' + roomNameWithoutSpace;
+    navigate(`/room/${room_id}`);
+  }
+
+  const joinRoom = (e) => {
+    e.preventDefault();
+    const hasSpaces = Boolean(userName.indexOf(' '))
+    if (userName === undefined || !hasSpaces || !invitationLink.includes("-") || !invitationLink.includes("@")) {
+      setJoined(false)
+      navigate('/invalid')
+    }
+    setUserID(uuid())
+    setJoined(true)
+    navigate(`/room/${invitationLink}`);
+  }
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/devs' element={<FindDev />} />
+        <Route path="/room" element={
+          !joined && <LandingPage createRoom={createRoom} userName={userName} setUserName={setUserName} roomName={roomName} setRoomName={setRoomName} joinRoom={joinRoom} invitationLink={invitationLink} setinvitationLink={setinvitationLink} />
+        } />
+        <Route path="/room/:ROOMID" element={<Room joined={joined} setJoined={setJoined} userID={userID} userName={userName} roomName={roomName} setUserName={setUserName} setRoomName={setRoomName} setinvitationLink={setinvitationLink}/>} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
